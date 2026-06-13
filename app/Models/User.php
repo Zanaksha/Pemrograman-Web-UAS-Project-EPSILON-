0ebaf24db42fb0a624ae2c0b6a1dc8fb97f042eb
Farrel Octavian <farreloctavian32@gmail.com>
2026-06-13 13:11:38 +0700

    resolve conflicts

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,16 +18,10 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-<<<<<<< HEAD
     'name',
     'email',
     'password',
     'last_seen',
-=======
-        'name',
-        'email',
-        'password',
->>>>>>> a312fddf1c54c060e6fe6f65d67bf1c7575797a3
     ];
 
     /**
